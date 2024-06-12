c0fc6efaa224c028ae16191d88c28efdefb1305d
Nader Ebrahim <nebrahim2424@gmail.com>
2024-06-12 14:45:39 -0700

    converted to mapbox.gl

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Map from "./components/Map";
+
 function App() {
   return (
     <div className="App">
-      <h1>React App</h1>
+      <Map />
     </div>
   );
 }
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,33 @@
-import GoogleMapReact from "google-map-react";
+import React from "react";
+import Map, { Marker } from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css"; // Import the CSS
+import { Icon } from "@iconify/react";
+import locationIcon from "@iconify/icons-mdi/fire-alert";
 
-const Map = () => {
-  return <div></div>;
+const mapboxAccessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
+
+const MapComponent = () => {
+  const [viewport, setViewport] = React.useState({
+    longitude: -118.2426,
+    latitude: 34.0549,
+    zoom: 6,
+  });
+
+  return (
+    <div className="map" style={{ height: "100vh", width: "100%" }}>
+      <Map
+        initialViewState={viewport}
+        style={{ width: "100%", height: "100%" }}
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+        mapboxAccessToken={mapboxAccessToken}
+        onViewportChange={(nextViewport) => setViewport(nextViewport)}
+      >
+        <Marker latitude={viewport.latitude} longitude={viewport.longitude}>
+          <Icon icon={locationIcon} className="location-icon" />
+        </Marker>
+      </Map>
+    </div>
+  );
 };
 
-export default Map;
+export default MapComponent;
